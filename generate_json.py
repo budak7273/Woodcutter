61bae9f229c535e7516308be4e0383be1c94f046
Rob B <computerguy440+gh@gmail.com>
2020-07-14 21:03:17 -0400

    Support optional overrides for log names and stripped wood names, therefore adding support for Crimson Stem and Warped Stem processing

--- generate_json.py
+++ generate_json.py
@@ -1,10 +1,14 @@
 import os
 
 WORD_TO_REPLACE = "WOOD"
+WORD_TO_REPLACE_LOG = "WOOD_log"
+WORD_TO_REPLACE_STRIPPED_WOOD = "WOOD_wood"
 TEMPLATES_DIR_RELATIVE = "\\templates"
 OUTPUT_DIR_RELATIVE = "\\data\\woodcutter\\recipes"
 #ADVANCEMENT_DIR_RELATIVE = "\\data\\woodcutter\\advancements\\recipes\\misc"
-woods = ['oak', 'spruce', 'birch', 'dark_oak', 'acacia', 'jungle', 'warped', 'crimson']
+woods = ['oak', 'spruce', 'birch', 'dark_oak', 'acacia', 'jungle', ('warped', 'warped_stem', 'warped_hyphae'), ('crimson', 'crimson_stem', 'crimson_hyphae')]
+# format for each wood item is ('<plank/generic>', <optional_log_and_stripped_override>, <optional_stripped_wood_override>)
+# if specifying a optional_stripped_wood_override you must also specify a optional_log_and_stripped_override
 
 currentDir = os.path.dirname(os.path.abspath(__file__))
 templatesDir = currentDir + TEMPLATES_DIR_RELATIVE
@@ -37,13 +41,24 @@ for file in templateFiles:
                                 #print(line, end='')
                 #print(newLines)
                 for variant in woods:
-                        print("\t" + variant)
-                        newFileName = file.replace(WORD_TO_REPLACE, variant)
-                        with open(outputDir + "\\" + newFileName, "w+") as newf:
+                        has_overrides = type(variant) is tuple
+                        
+                        primary_name = variant[0] if has_overrides else variant
+                        log_and_stripped_override_name = variant[1] if has_overrides else None
+                        stripped_wood_override_name = variant[2] if has_overrides else None
+                        
+                        print("\t" + primary_name + " (has overrides: " + str(has_overrides) + ")")
+                        newFileName = file.replace(WORD_TO_REPLACE, primary_name)
+                        with open(outputDir + "\\" + newFileName, "w+") as newfile:
                                 for line in newLines:
-                                        temp = line.replace(WORD_TO_REPLACE, variant)
-                                        #print("Writing: " + temp)
-                                        newf.write(temp)
+                                        resultLine = line
+                                        if has_overrides: #first replace the more specific lines, then the general ones
+                                            resultLine = resultLine.replace(WORD_TO_REPLACE_LOG, log_and_stripped_override_name)
+                                            resultLine = resultLine.replace(WORD_TO_REPLACE_STRIPPED_WOOD, stripped_wood_override_name)
+                                        #regardless of overrides, replace WOOD with the wood variant
+                                        resultLine = resultLine.replace(WORD_TO_REPLACE, primary_name)
+                                        #print("Writing: " + resultLine)
+                                        newfile.write(resultLine)
                         #print("appending " + newFileName + " to " + variant + " recipe list")
                         #eachVariantRecipeNames[variant].append(newFileName)
                         
