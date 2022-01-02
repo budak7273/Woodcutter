25d8872f4837e7e545fc84d2c4282e7d71877376
Robb <computerguy440+gh@gmail.com>
2022-01-02 03:00:04 -0600

    Add support for sapling overrides and ignoring boat related templates

--- generate_json.py
+++ generate_json.py
@@ -3,12 +3,13 @@ import os
 WORD_TO_REPLACE = "WOOD"
 WORD_TO_REPLACE_LOG = "WOOD_log"
 WORD_TO_REPLACE_STRIPPED_WOOD = "WOOD_wood"
+WORD_TO_REPLACE_SAPLING = "WOOD_sapling"
 TEMPLATES_DIR_RELATIVE = "\\templates"
 OUTPUT_DIR_RELATIVE = "\\data\\woodcutter\\recipes"
 #ADVANCEMENT_DIR_RELATIVE = "\\data\\woodcutter\\advancements\\recipes\\misc"
-woods = ['oak', 'spruce', 'birch', 'dark_oak', 'acacia', 'jungle', ('warped', 'warped_stem', 'warped_hyphae'), ('crimson', 'crimson_stem', 'crimson_hyphae')]
-# format for each wood item is ('<plank/generic>', <optional_log_and_stripped_override>, <optional_stripped_wood_override>)
-# if specifying a optional_stripped_wood_override you must also specify a optional_log_and_stripped_override
+woods = ['oak', 'spruce', 'birch', 'dark_oak', 'acacia', 'jungle', ('warped', 'warped_stem', 'warped_hyphae', 'warped_fungus'), ('crimson', 'crimson_stem', 'crimson_hyphae', 'crimson_fungus')]
+# format for each wood item is ('<plank/generic>', <optional_log_and_stripped_override>, <optional_stripped_wood_override>, <optional_sapling_override>)
+# if specifying any optional fields, you must specify them all, so that order is maintained
 
 currentDir = os.path.dirname(os.path.abspath(__file__))
 templatesDir = currentDir + TEMPLATES_DIR_RELATIVE
@@ -29,7 +30,7 @@ if(not(os.path.exists(outputDir))):
 #eachVariantRecipeNames = []
 #for variant in woods:
 #        eachVariantRecipeNames["" + variant] = []
-        
+
 for file in templateFiles:
         if ".json" in file:
                 print('Processing recipe template ' + file )
@@ -40,31 +41,35 @@ for file in templateFiles:
                                 newLines.append(line)
                                 #print(line, end='')
                 #print(newLines)
+
                 for variant in woods:
                         has_overrides = type(variant) is tuple
-                        
+
                         primary_name = variant[0] if has_overrides else variant
                         log_and_stripped_override_name = variant[1] if has_overrides else None
                         stripped_wood_override_name = variant[2] if has_overrides else None
-                        
-                        print("\t" + primary_name + " (has overrides: " + str(has_overrides) + ")")
+                        sapling_override_name = variant[3] if has_overrides else None
+
+                        print("\t" + primary_name + " (has overrides?: " + str(has_overrides) + ")")
                         newFileName = file.replace(WORD_TO_REPLACE, primary_name)
+                        if "boat" in newFileName and has_overrides:
+                            break # override woods don't have boat variants, so don't generate boat related recipes for them
                         with open(outputDir + "\\" + newFileName, "w+") as newfile:
                                 for line in newLines:
                                         resultLine = line
-                                        if has_overrides: #first replace the more specific lines, then the general ones
+                                        if has_overrides: # first, replace the more specific lines, then the general ones
                                             resultLine = resultLine.replace(WORD_TO_REPLACE_LOG, log_and_stripped_override_name)
                                             resultLine = resultLine.replace(WORD_TO_REPLACE_STRIPPED_WOOD, stripped_wood_override_name)
-                                        #regardless of overrides, replace WOOD with the wood variant
+                                            resultLine = resultLine.replace(WORD_TO_REPLACE_SAPLING, sapling_override_name)
+                                        # regardless of overrides, replace WOOD with the wood variant
                                         resultLine = resultLine.replace(WORD_TO_REPLACE, primary_name)
                                         #print("Writing: " + resultLine)
                                         newfile.write(resultLine)
                         #print("appending " + newFileName + " to " + variant + " recipe list")
                         #eachVariantRecipeNames[variant].append(newFileName)
-                        
+
 #for variant in woods:
 #        print("Writing advancement for " + variant")
-        
-        
-        
-	
+
+print("Generation complete, copy the contents of this folder to a datapack (ex `<save folder>/datapacks/woodcutter` and use `/datapack enable \"file/woodcutter\"` to enable")
+print("Remember to delete the output directory if you're changed template file names and are re-running the script!")
